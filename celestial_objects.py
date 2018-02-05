1abe31dac761bbeedc5bc9dc11e9bc05637cc259
Nick Walsh <nick.walsh96@outlook.com>
2018-02-05 16:45:05 -0600

    Started working on classes

---
+++ celestial_objects.py
@@ -0,0 +1,47 @@
+from abc import ABCMeta, abstractmethod
+
+
+class BaseCelestialObject:
+    __metaclass__ = ABCMeta
+    def __init__(self):
+        self.right_ascension = None
+        self.declination = None
+
+    # example of how to do an abstract method
+    # @abstractmethod
+    # def example(self):
+    #     pass
+
+class Planet(BaseCelestialObject):
+    def __init__(self):
+        BaseCelestialObject.__init__(self)
+        self.planet_name = None
+
+class Star(BaseCelestialObject):
+    def __init__(self):
+        BaseCelestialObject.__init__(self)
+        self.proper_name = None
+        self.star_id = None
+        self.magnitude = None
+        self.hd_number = None
+
+class Moon(BaseCelestialObject):
+    def __init__(self):
+        BaseCelestialObject.__init__(self)
+        self.phase = None
+
+class Constellation(BaseCelestialObject):
+    def __init__(self):
+        BaseCelestialObject.__init__(self)
+        self.name = None
+        self.number_stars = None
+        self.number_lines = None
+        self.stars = []
+
+class Messier_Objects(BaseCelestialObject)
+    def __init__(self):
+        BaseCelestialObject.__init__(self)
+
+
+
+

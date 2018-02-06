70cb1c0ccc490c94723e47196c51e333f8791118
Nick Walsh <nick.walsh96@outlook.com>
2018-02-06 11:18:57 -0600

    julian date and gmst conversions

--- calculations.py
+++ calculations.py
@@ -1,4 +1,6 @@
 import math
+import astropy.time
+import dateutil.parser
 
 year = 2018
 month = 2
@@ -85,3 +87,58 @@ def convert_dec_dms(dec):
     minutes = int((dec - degrees) * 60.0)
     seconds = (((dec - degrees) * 60.0) - minutes) * 60.0
     return (degrees, minutes, seconds)
+
+def calculate_julian_day(year, month, day, hour, minute, second):
+    julian_day = ((367 * year) - math.floor(7.0 * (year + math.floor((month + 9.0) / 12.0) / 4.0)) + math.floor(275.0 * month / 9.0) + day - 730531.5 + hour / 24.0)
+    return julian_day
+
+def calculate_julian_day_astropy(year, month, day):
+    dt = dateutil.parser.parse(str(year) + '.' + str(month) + '.' + str(day))
+    time = astropy.time.Time(dt)
+    return time.jd
+
+def calculate_julian_day_coleman(year, month, day, hour, minute):
+    converted_time = hour + (minute / 60)
+    converted_day = day + (converted_time / 24)
+
+    if month > 2:
+        year = year
+        month = month
+    else:
+        year -= 1
+        month += 12
+
+    b = -13
+
+    jd = int(365.25 * year) + int(30.6001 * (month + 1)) + converted_day + 1720994.5 + b
+    return jd
+
+def calculate_gmst(year, month, day, hour, minute):
+    jd = calculate_julian_day_coleman(year, month, day, hour, minute)
+    midnight = math.floor(jd) + 0.5
+    days_since_midnight = jd - midnight
+    hours_since_midnight = days_since_midnight * 24
+    days_since_epoch = jd - 2451545.0
+    centuries_since_epoch = days_since_epoch / 35625
+    whole_days_since_epoch = midnight - 2451545.0
+
+    gmst = 6.697374558 + 0.06570982441908 * whole_days_since_epoch + 1.00273790935 * hours_since_midnight + 0.000026 * math.pow(centuries_since_epoch, 2)
+    gmst_remainder = gmst % int(gmst)
+    gmst_hours = math.floor(gmst) % 24
+    gmst_minutes = math.floor(gmst_remainder * 60)
+    gmst_minutes_decimal = gmst_remainder * 60
+    gmst_minutes_decimal = gmst_minutes_decimal % int(gmst_minutes_decimal)
+    gmst_seconds = gmst_minutes_decimal * 60
+
+    print(gmst_hours)
+    print(gmst_minutes)
+    print(gmst_seconds)
+
+
+
+
+if __name__ == "__main__":
+    print(calculate_julian_day_astropy(2019, 1, 1))
+    print(calculate_julian_day_coleman(2019, 1, 1, 8, 0))
+    calculate_gmst(2004, 1, 1, 0, 0)
+

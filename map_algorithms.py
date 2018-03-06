a811ad61da1021bf74d826c9c8e2fb59f4eacc40
Joanna-G <allinmythread@gmail.com>
2018-03-06 08:38:00 -0600

    Implemented the algorithms. Still no idea what I'm doing.

--- map_algorithms.py
+++ map_algorithms.py
@@ -10,7 +10,7 @@ import math
 
 # I think that the "longitude" is the azimuth in this case. It would go from 0 at "North" (East?) working
 # counterclockwise like a normal circle.
-# I think the "latitude" is the altitude. It'd go from 90 in the center to 0 at the edge of the graph.
+# I think the "latitude" is the altitude. It'd go from 90 in the center (zenith) to 0 at the edge (horizon).
 # The following call for some sort of "center" and I'm not entirely sure what that would be. I think we
 # would say the center is 90 latitude, 0 longitude?
 
@@ -22,6 +22,8 @@ def alt_az_to_rect(alt, az, r):
     y = (r * math.cos(math.radians(az)) * math.sin(math.radians(alt)))
     z = (r * math.sin(math.radians(az)))
 
+    # Print for testing
+    print("x = " + str(x) + " y = " + str(y) + " z = " + str(z))
     return x, y, z
 
 
@@ -30,6 +32,8 @@ def rect_to_polar(x, y):
     r = math.sqrt(x * x + y * y)
     theta = math.atan(y / x)
 
+    # Print for testing
+    print("r = " + str(r) + " theta = " + str(theta))
     return r, theta
 
 
@@ -52,6 +56,9 @@ def azimuthal_equidistant(az, alt):
     # y = kp * (cos(phi1) * sin(phi) - sin(phi1) * cos(phi) * cos(lambda - lambda0))
     y = kp * (math.cos(phi1) * math.sin(phi) - math.sin(phi1) * math.cos(phi) * math.cos(lamb - lamb0))
 
+    # Print for testing
+    print("x = " + str(x) + " y = " + str(y))
+
     return x, y
 
 
@@ -71,6 +78,9 @@ def lambert_az_equidistant(az, alt):
     # y = kp * (cos(phi1) * sin(phi) - sin(phi1) * cos(phi) * cos(lambda - lambda0))
     y = kp * (math.cos(phi1) * math.sin(phi) - math.sin(phi1) * math.cos(phi) * math.cos(lamb - lamb0))
 
+    # Print for testing
+    print("x = " + str(x) + " y = " + str(y))
+
     return x, y
 
 
@@ -85,12 +95,26 @@ def stereographic(az, alt):
     phi = math.radians(90 - alt)
     lamb = math.radians(az)
 
-    # k = (2 * R) / (1 + sin(phi1) * sin(phi) + cos(phi1) * cos(phi) * cos(lambda - lambda0))
+    # k = (2 * r) / (1 + sin(phi1) * sin(phi) + cos(phi1) * cos(phi) * cos(lambda - lambda0))
     k = (2 * r) / (1 + math.sin(phi1) * math.sin(phi) + math.cos(phi1) * math.cos(phi) * math.cos(lamb - lamb0))
     # x = k * cos(phi) * sin(lambda - lambda0)
     x = k * math.cos(phi) * math.sin(lamb - lamb0)
     # y = k * (cos(phi1) * sin(phi) - sin(phi1) * cos(phi) * cos(lambda - lambda0))
     y = k * (math.cos(phi1) * math.sin(phi) - math.sin(phi1) * math.cos(phi) * math.cos(lamb - lamb0))
 
+    # Print for testing
+    print("x = " + str(x) + " y = " + str(y))
+
     return x, y
 
+
+# Local variables for testing
+alt = 20
+az = 270
+r = 1
+
+x, y, z = alt_az_to_rect(alt, az, r)
+r, theta = rect_to_polar(x, y)
+x, y = azimuthal_equidistant(az, alt)
+x, y = lambert_az_equidistant(az, alt)
+x, y = stereographic(az, alt)
